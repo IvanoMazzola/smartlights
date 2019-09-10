4ce217d81d4a8b0c5eabe19f75595cf7eeb5bfe3
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-09-10 16:08:00 +0200

    Added font - show filters in dashboard

--- src/app/members/dashboard/dashboard.page.ts
+++ src/app/members/dashboard/dashboard.page.ts
@@ -1,7 +1,9 @@
 import { AuthenticationService } from './../../services/authentication.service';
 import { FilterService } from './../../services/filter.service';
+import { ToastService } from './../../services/toast.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { db } from '../../appdb';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,7 +14,11 @@ export class DashboardPage implements OnInit {
 
   items: any[] = [];
 
-  constructor(private authService: AuthenticationService, private router: Router, private filterService: FilterService) {
+  consumptionFilter: string[] = [];
+  areaFilter: string[] = [];
+  statusFilter: string[] = [];
+
+  constructor(private authService: AuthenticationService, private router: Router, private filterService: FilterService, private toastService: ToastService) {
 
   }
 
@@ -20,7 +26,38 @@ export class DashboardPage implements OnInit {
 
   }
 
+  updateStatus(event) {
+    console.log(event);
+    const plantID = event.srcElement.id;
+    this.items.forEach(x => {
+      console.log(x);
+      // tslint:disable-next-line: triple-equals
+      if (x.id == plantID) {
+        console.log('Status ' + x.status);
+        if (x.status === 'OFF') {
+          db.table('plants').update(x.id, { status: 'ON' }).then(updated => {
+            if (updated) {
+              console.log('Aggiornato');
+              x.status = 'ON';
+            }
+          });
+        } else {
+          db.table('plants').update(x.id, { status: 'OFF' }).then(updated => {
+            if (updated) {
+              console.log('Aggiornato');
+              x.status = 'OFF';
+            }
+          });
+        }
+      }
+    });
+  }
+
+
   ionViewWillEnter() {
+    if (this.filterService.consumption.length !== 3) {
+      this.consumptionFilter = this.filterService.consumption;
+    }
     this.filterService.getPlants().then(plants => this.items = plants);
   }
 
