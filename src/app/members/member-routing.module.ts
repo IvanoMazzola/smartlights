e5b187886e012c521a182bf66929c084fc51d2be
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-09-17 16:47:09 +0200

    Tabs working

--- src/app/members/member-routing.module.ts
+++ src/app/members/member-routing.module.ts
@@ -5,7 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
   { path: 'filters', loadChildren: './filters/filters.module#FiltersPageModule' },
-  { path: 'plant', loadChildren: './plant/plant.module#PlantPageModule' }
+  { path: 'plant', loadChildren: './plant/plant.module#PlantPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' }
 ];
 
 
